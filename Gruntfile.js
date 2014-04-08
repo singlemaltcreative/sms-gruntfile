dec7c9f060ae210ebc5a790b1c1b6a363cc66864
Paul Bredenberg <paulbredenberg@gmail.com>
2014-04-08 17:35:16 -0400

    Initial grunt file, package, and docs.

---
+++ Gruntfile.js
@@ -0,0 +1,98 @@
+'use strict';
+
+var LIVERELOAD_PORT = 35729;
+var currIP = '192.168.1.117';
+var vHost = 'myfi.dev';
+
+module.exports = function(grunt) {
+	
+	require('time-grunt')(grunt);
+	require('jit-grunt')(grunt);
+
+	grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+		
+		watch: {
+		    scripts: {
+		        files: ['js/*.js'],
+		        tasks: ['concat'],
+		        options: {
+		            spawn: false,
+		            livereload: LIVERELOAD_PORT,		            
+		        },
+		    },
+		    css: {
+			    files: ['css/src/*.scss'],
+			    tasks: ['sass'],
+			},
+			markup: {
+				files: [
+					'**/*.html',
+					'*.html',
+				],
+				options: {
+		            spawn: false,
+		            livereload: LIVERELOAD_PORT,		            
+		        },
+			}
+		},
+		
+		concat: {
+			plugins: {
+				src: [
+					'js/src/*.js'
+				],
+				dest: 
+					'js/plugins.js'
+				,
+			},
+			vendor: {
+				src: [
+					'js/vendor/*.js',
+				],
+				dest: 
+					'js/vendor.js'
+				,
+			},
+		},
+
+		uglify: {
+		    my_target: {
+		      files: {
+		        'js/production.min.js': ['js/vendor.js', 'js/plugins.js', 'js/custom.js']
+		      }
+		    }
+		},
+		
+		sass: {
+		    compile: {
+				files: {
+					'css/combined.css': 'css/src/combined.scss'
+				}
+			},
+		},
+		
+		browserSync: {
+			dev: {
+			    bsFiles: {
+			        src : [
+			        	'css/combined.css',
+			        	'*.html',
+			        	'*/*.html',
+		        	]
+			    }, 
+			    options: {
+		            watchTask: true,
+		            debugInfo: true,
+		            host: vHost + '.' + currIP + '.xip.io',
+		        }
+			}
+		}
+
+    });
+	
+	grunt.registerTask('default', ['browserSync', 'watch']);
+
+	grunt.registerTask('compile', ['uglify', 'sass']);
+
+};
--- README.md
+++ README.md
@@ -2,3 +2,47 @@ sms-gruntfile
 =============
 
 Single Malt Sites' custom Gruntfile.js and associated package.json file, which we use when beginning all of our projects.
+
+#SMS Grunt Configuration
+
+We thought we'd post our current Grunt.js configuration, which is the result of hours of research, trial and error, and new gray hairs.
+
+##Required Variable Definitions
+
+###LIVERELOAD_PORT
+
+Needed if you experience any port conflicts or other weirdness with LiveReload on your system.
+
+###currIP
+
+To define your current IP address when using xip.io with MAMP Pro or other virtual host setup.
+
+##Installation
+
+###For the Gruntless:
+
+	npm install -g grunt-cli
+
+This configuration requires at least Ruby 2.1.1, and the native Sass. To accomplish this on OS X I've used RVM.
+
+Consider [this article](http://rvm.io/rvm/install)
+
+###For the Grunted:
+
+Copy Gruntfile.js and package.json into your project directory, navigate to your project directory and run:
+
+	npm install
+
+Then in your directory, run:
+
+	grunt
+
+That *should* be it. (see below)
+
+##Notes
+
+This is the very first push, my friends. I have yet to test it! I will test it soon hopefully and update this doc accordingly.
+	
+		
+
+
